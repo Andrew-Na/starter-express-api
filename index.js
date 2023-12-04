216b8865b32420a9d75fddd83637b4f25358c7c1
Andrew-Na <47693154+Andrew-Na@users.noreply.github.com>
2023-12-03 22:37:27 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,52 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+// const express = require('express')
+// const app = express()
+// app.all('/', (req, res) => {
+//     console.log("Just got a request!")
+//     res.send('Yo!')
+// })
+// app.listen(process.env.PORT || 3000)
+const HTTP_PORT = process.env.PORT || 8080;
+
+// JSON Web Token Setup
+let ExtractJwt = passportJWT.ExtractJwt;
+let JwtStrategy = passportJWT.Strategy;
+
+// Configure its options
+let jwtOptions = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderWithScheme('jwt'),
+  secretOrKey: '&0y7$noP#5rt99&GB%Pz7j2b1vkzaB0RKs%^N^0zOP89NT04mPuaM!&G8cbNZOtH',
+};
+
+// IMPORTANT - this secret should be a long, unguessable string
+// (ideally stored in a "protected storage" area on the web server).
+// We suggest that you generate a random 50-character string
+// using the following online tool:
+// https://lastpass.com/generatepassword.php
+
+let strategy = new JwtStrategy(jwtOptions, function (jwt_payload, next) {
+    console.log('payload received', jwt_payload);
+
+    if (jwt_payload) {
+        // The following will ensure that all routes using 
+        // passport.authenticate have a req.user._id, req.user.userName, req.user.fullName & req.user.role values 
+        // that matches the request payload data
+        next(null, { _id: jwt_payload._id, 
+            userName: jwt_payload.userName, 
+            fullName: jwt_payload.fullName, 
+            role: jwt_payload.role }); 
+    } else {
+        next(null, false);
+    }
+});
+
+// tell passport to use our "strategy"
+passport.use(strategy);
+
+// add passport as application-level middleware
+app.use(passport.initialize());
+
+app.use(cors());
+app.use(express.json());
+app.get("/api/", (req, res) => {
+   res.json({ "message": "login successful"});
+});
