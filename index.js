3a4e41b5678ded60544b2a60a139045a8c7a39c2
Andrew-Na <47693154+Andrew-Na@users.noreply.github.com>
2023-12-03 22:42:25 -0500

    Update index.js

--- index.js
+++ index.js
@@ -5,24 +5,24 @@
 //     res.send('Yo!')
 // })
 // app.listen(process.env.PORT || 3000)
-const express = require("express");
-const cors = require("cors");
-const userService = require("./user-services.js");
-const jwt = require('jsonwebtoken');
-const passport = require("passport");
-const passportJWT = require("passport-jwt");
-const app = express();
-const HTTP_PORT = process.env.PORT || 3000;
+const express = require("express")
+const cors = require("cors")
+const userService = require("./user-services.js")
+const jwt = require('jsonwebtoken')
+const passport = require("passport")
+const passportJWT = require("passport-jwt")
+const app = express()
+const HTTP_PORT = process.env.PORT || 3000
 
 // JSON Web Token Setup
-let ExtractJwt = passportJWT.ExtractJwt;
-let JwtStrategy = passportJWT.Strategy;
+let ExtractJwt = passportJWT.ExtractJwt
+let JwtStrategy = passportJWT.Strategy
 
 // Configure its options
 let jwtOptions = {
   jwtFromRequest: ExtractJwt.fromAuthHeaderWithScheme('jwt'),
   secretOrKey: '&0y7$noP#5rt99&GB%Pz7j2b1vkzaB0RKs%^N^0zOP89NT04mPuaM!&G8cbNZOtH',
-};
+}
 
 // IMPORTANT - this secret should be a long, unguessable string
 // (ideally stored in a "protected storage" area on the web server).
@@ -31,7 +31,7 @@ let jwtOptions = {
 // https://lastpass.com/generatepassword.php
 
 let strategy = new JwtStrategy(jwtOptions, function (jwt_payload, next) {
-    console.log('payload received', jwt_payload);
+    console.log('payload received', jwt_payload)
 
     if (jwt_payload) {
         // The following will ensure that all routes using 
@@ -40,20 +40,20 @@ let strategy = new JwtStrategy(jwtOptions, function (jwt_payload, next) {
         next(null, { _id: jwt_payload._id, 
             userName: jwt_payload.userName, 
             fullName: jwt_payload.fullName, 
-            role: jwt_payload.role }); 
+            role: jwt_payload.role })
     } else {
-        next(null, false);
+        next(null, false)
     }
 });
 
 // tell passport to use our "strategy"
-passport.use(strategy);
+passport.use(strategy)
 
 // add passport as application-level middleware
-app.use(passport.initialize());
+app.use(passport.initialize())
 
-app.use(cors());
-app.use(express.json());
+app.use(cors())
+app.use(express.json())
 app.get("/api", (req, res) => {
    //res.json({ "message": "login successful"});
   res.send('test')
